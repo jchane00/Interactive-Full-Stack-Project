38190fb273a0238483b3fc6b3d85a33f89296223
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-08-09 00:39:50 -0500

    updated models

---
+++ models/Cars.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Cars extends Model {}
+
+Cars.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vin_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Cars',
+  }
+);
+
+module.exports = Cars;
